ac8f5c140a1538db3067a3b711a67f3e20beb630
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmaail.com>
2023-05-03 20:40:44 -0300

    OAuth2 JWT checklist

---
+++ src/main/java/br/com/vinicius/ecommerce/config/ResourceServerConfig.java
@@ -0,0 +1,29 @@
+package br.com.vinicius.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // H2 frames
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
