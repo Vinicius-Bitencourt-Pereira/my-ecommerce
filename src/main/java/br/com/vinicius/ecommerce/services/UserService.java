4960aa20b5e35570301cb9cd2fb378f6c0be6cb2
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmaail.com>
2023-05-04 18:26:50 -0300

    Getting user logged

--- src/main/java/br/com/vinicius/ecommerce/services/UserService.java
+++ src/main/java/br/com/vinicius/ecommerce/services/UserService.java
@@ -1,12 +1,15 @@
 package br.com.vinicius.ecommerce.services;
 
+import br.com.vinicius.ecommerce.dto.UserDTO;
 import br.com.vinicius.ecommerce.entities.User;
 import br.com.vinicius.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -22,5 +25,21 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException("Email not found");
         }
         return user;
+    }
+
+    protected User authenticated(){
+        try{
+            String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+            return repository.findByEmail(userName);
+        }
+        catch (Exception e){
+            throw  new UsernameNotFoundException("Invalid user");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User entity = authenticated();
+        return new UserDTO(entity);
     }
 }
