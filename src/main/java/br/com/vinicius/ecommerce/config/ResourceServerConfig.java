2a3c06112443c95920498f7d578d70aba4a704cd
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmaail.com>
2023-05-04 17:31:52 -0300

    Environment variables

--- src/main/java/br/com/vinicius/ecommerce/config/ResourceServerConfig.java
+++ src/main/java/br/com/vinicius/ecommerce/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package br.com.vinicius.ecommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,8 +12,9 @@ import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    
+
     @Autowired
     private Environment env;
 
